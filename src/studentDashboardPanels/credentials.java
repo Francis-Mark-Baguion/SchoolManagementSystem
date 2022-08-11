e13b28fb7b791908d60e5df2d1542d52ff4fbc90
fmbags <baguion445@gmail.com>
2022-08-11 08:06:47 +0800

    Modification and fixes

---
+++ src/studentDashboardPanels/credentials.java
@@ -0,0 +1,16 @@
+package studentDashboardPanels;
+
+public class credentials {
+
+	private int id;
+	
+	public int getid() {
+		return id;
+	}
+	
+	public void setid(int id) {
+		this.id = id;
+	}
+	
+}
+
