8c570e87d8a55a261687f800605cf42319eff8e9
fmbags <baguion445@gmail.com>
2022-08-19 18:33:56 +0800

    clean up from practice

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,5 @@ public class Main {
 	public static void main(String[] args) {
 		
 		HomeFrame homeframe = new HomeFrame();
-		
-		System.out.println("merge test again");
-		System.out.println("conflict test again");
-		System.out.println("conflict test again");
-		System.out.println("conflict test again");
-		System.out.println("hello world");
-		System.out.println("rebase test, from main");
 	}
 }
