7fcf4ed6677c36ac36d54a2cfff8a0b46a4ad6cb
fmbags <baguion445@gmail.com>
2022-08-29 21:22:31 +0800

    left out edits

--- src/adminDashboardPanels/AdminMessagesPanel.java
+++ src/adminDashboardPanels/AdminMessagesPanel.java
@@ -1,5 +1,4 @@
 package adminDashboardPanels;
-
 import javax.swing.JPanel;
 
 public class AdminMessagesPanel extends JPanel {
@@ -8,7 +7,8 @@ public class AdminMessagesPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public AdminMessagesPanel() {
-
+		setBounds(0, 0, 985, 609);
+		
 	}
 
 }
