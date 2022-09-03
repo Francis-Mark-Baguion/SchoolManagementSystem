e2424b85c71229f0a6e4031bf5f41db8afe82bde
fmbags <baguion445@gmail.com>
2022-09-03 21:36:23 +0800

    clean_up 2

--- src/StudentLogIn.java
+++ src/StudentLogIn.java
@@ -1,4 +1,3 @@
-
 import java.awt.BorderLayout;
 import java.sql.*;
 import java.awt.Color;
@@ -8,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
